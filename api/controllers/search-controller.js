b00f994ec671b004bc753ea171f7139cffd83fae
Tash-had Saqif <me@tash-had.com>
2018-07-19 20:34:10 -0400

    finished /search endpoint - requests, parses, and returns courses matching a given query

--- api/controllers/search-controller.js
+++ api/controllers/search-controller.js
@@ -3,8 +3,9 @@
 var searchService = require("../services/search-service"); 
 
 exports.searchCourse = function(req, res){
-    var response = searchService.sendQueryToGriddy(req.query.searchQuery, function(data){
-        res.send(data.CodeNameMatches); 
+    searchService.sendQueryToGriddy(req.query.searchQuery, function(data){
+        var matchingCourses = searchService.parseGriddyResponse(data); 
+        res.send(matchingCourses); 
     }); 
 };
 
--- api/services/search-service.js
+++ api/services/search-service.js
@@ -1,12 +1,27 @@
-'use strict'; 
+'use strict';
 
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
 module.exports = {
-    sendQueryToGriddy : function (query, onRequestCompleted){
+    sendQueryToGriddy: function (query, onRequestCompleted) {
         client.get("http://griddy.org/api/course?q=" + query, function (data) {
             onRequestCompleted(data);
         });
+    },
+    parseGriddyResponse: function (data) {
+        var resultsArr = data.CodeNameMatches.split("</li>");
+        var matchingCourses = []
+        for (var i = 0; i < resultsArr.length - 1; i++) { // last element is always ""
+            var courseAsListItem = resultsArr[i];
+            var courseDetails = courseAsListItem.substring(courseAsListItem.indexOf(">") + 1);
+            courseAsListItem = courseAsListItem + "</li>"; // end tag got deleted in the split
+            var course = {
+                course: courseDetails,
+                html: courseAsListItem
+            };
+            matchingCourses.push(course);
+        }
+        return matchingCourses; 
     }
 }
