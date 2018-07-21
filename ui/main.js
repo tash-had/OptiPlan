050e777416e9a2bd7e387c77e2b6734e0d805178
Labib <labibc01@gmail.com>
2018-07-20 22:53:22 -0400

    minor style changes

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,38 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     var course = '';
     $('#courseSearch').keypress(function (e) {
         var regex = new RegExp("^[a-zA-Z0-9]+$");
         var char = String.fromCharCode(!e.charCode ? e.which : e.charCode);
         if (e.charCode == 8 || regex.test(char)) {
             console.log(char);
-            course = this.value+char;
-            if (course.length > 2){
+            course = this.value + char;
+            if (course.length > 2) {
                 var reg2 = new RegExp("^[a-zA-Z]{3}[0-9]{1,3}$");
-                if (reg2.test(course)){
-                    var URL = 'http://localhost:3000/search-course?searchQuery='+course;
-                    httpGetAsync(URL, function(response){
-                    console.log(response);
+                if (reg2.test(course)) {
+                    var URL = 'http://localhost:3000/search-course?searchQuery=' + course;
+                    httpGetAsync(URL, function (response) {
+                        console.log(response);
                     });
                 }
 
             }
             return true;
         }
-    
+
         e.preventDefault();
         return false;
     });
-    $("#btn").on('click', function(e){ //Submit still doesnt get updated after backspace
-        if (course.length > 2){
+    $("#btn").on('click', function (e) { //Submit still doesnt get updated after backspace
+        if (course.length > 2) {
             console.log(course);
         }
-        
+
     });
 
-    function httpGetAsync(theUrl, callback)
-    {
+    function httpGetAsync(theUrl, callback) {
         var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() { 
+        xmlHttp.onreadystatechange = function () {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
                 callback(xmlHttp.responseText);
         }
