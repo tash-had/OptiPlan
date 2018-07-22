d6334fd971dfdcd072e16fab2d8e9df919cebcf7
Tash-had Saqif <me@tash-had.com>
2018-07-22 18:44:50 -0400

    format js files

--- api/controllers/course-data-controller.js
+++ api/controllers/course-data-controller.js
@@ -1,11 +1,11 @@
 'use-strict';
 
-var courseDataService = require("../services/course-data-service"); 
+var courseDataService = require("../services/course-data-service");
 
-exports.getCourseData = function(req, res){
-    courseDataService.sendQueryToGriddy(req.query.courseId, function(data){
-        var matchingCourses = courseDataService.parseGriddyResponse(data); 
-        res.send(matchingCourses); 
-    }); 
+exports.getCourseData = function (req, res) {
+    courseDataService.sendQueryToGriddy(req.query.courseId, function (data) {
+        var matchingCourses = courseDataService.parseGriddyResponse(data);
+        res.send(matchingCourses);
+    });
 };
 
--- api/routes/optiplan-routes.js
+++ api/routes/optiplan-routes.js
@@ -1,10 +1,10 @@
-'use strict'; 
+'use strict';
 
-module.exports = function(app){
+module.exports = function (app) {
     var timetableController = require('../controllers/timetable-controller'),
-    searchController = require('../controllers/search-controller'),
-    courseDataController = require('../controllers/course-data-controller');
-    
+        searchController = require('../controllers/search-controller'),
+        courseDataController = require('../controllers/course-data-controller');
+
     // set routes 
     app.route("/timetable")
         .get(timetableController.generateTimetable)
@@ -12,7 +12,7 @@ module.exports = function(app){
 
     app.route("/search-course")
         .get(searchController.searchCourse)
-    
+
     app.route("/course")
         .get(courseDataController.getCourseData)
 }
