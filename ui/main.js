e6416d748c38c09f76d0b7ec2846c0c32240065e
Labib <labibc01@gmail.com>
2018-07-26 18:52:54 -0400

    put courses into an array

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,17 @@
 
 $(document).ready(function () {
+    var input = '';
     $('#courseSearch').keyup(function (e) {
         var regex = new RegExp("^[a-zA-Z0-9]+$");
-        var input = this.value;
+        input = this.value;
         if (regex.test(input)) {
             console.log(input);
-            if (course.length > 2) {
+            if (input.length > 2) {
                 var reg2 = new RegExp("^[a-zA-Z]{3}[0-9]{1,3}$");
-                if (reg2.test(course)) {
+                if (reg2.test(input)) {
                     var URL = 'http://localhost:3000/search-course?searchQuery=' + input;
                     $.get(URL, function (response) {
-                        console.log(response);
+                        getCourses(response);
                     });
                 }
 
@@ -22,9 +23,19 @@ $(document).ready(function () {
         return false;
     });
     $("#btn").on('click', function (e) { //Submit still doesnt get updated after backspace
-        if (course.length > 2) {
-            console.log(course);
+        if (input.length > 2) {
+            console.log(input);
         }
 
     });
 });
+
+function getCourses(data){
+    var course_array = [];
+    for (i in data){
+        course_array.push(data[i].course);
+        
+    }
+    console.log(course_array);
+    
+}
