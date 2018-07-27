2a2660c28d62c432a3ebe3b3595fad3fa6528bee
Tash-had Saqif <me@tash-had.com>
2018-07-27 18:54:05 -0400

    remove html from returned items

--- api/services/search-service.js
+++ api/services/search-service.js
@@ -2,47 +2,32 @@
 
 var Client = require('node-rest-client').Client;
 var client = new Client();
-const GRIDDY_BASE = "http://griddy.org/api/";
+const GRIDDY_BASE = "http://griddy.org/api";
 
 module.exports = {
-    ENDPOINTS: {
-        courseSearch: GRIDDY_BASE + "/course?q=",
-        courseDataSearch: GRIDDY_BASE + "/course?id="
-    },
-    sendQueryToGriddy: function (endpoint, params, onRequestCompleted) {
-        client.get(endpoint + params, function (data) {
+    sendQueryToGriddy: function (query, onRequestCompleted) {
+        client.get("http://griddy.org/api/course?q=" + query, function (data) {
             onRequestCompleted(data);
         });
     },
-    parseCourseSearchResults: function (data) {
+    parseGriddyResponse: function (data) {
         var resultsArr = data.CodeNameMatches.split("</li>");
         var matchingCourses = []
         for (var i = 0; i < resultsArr.length - 1; i++) { // last element is always ""
             var courseAsListItem = resultsArr[i];
-            var courseCode = courseAsListItem.substring(courseAsListItem.indexOf(">") + 1, courseAsListItem.indexOf(":"));
+            var courseName = courseAsListItem.substring(courseAsListItem.indexOf(">") + 1, courseAsListItem.indexOf(":"));
             var courseFullName = courseAsListItem.substring(courseAsListItem.indexOf(">") + 1);
-            var courseID = courseAsListItem.substring(courseAsListItem.indexOf('=') + 1, courseAsListItem.indexOf('class') - 1);
 
             courseAsListItem = courseAsListItem + "</li>"; // end tag got deleted in the split
             var course = {
-                course: courseCode,
+                course: courseName,
                 courseFullName: courseFullName,
                 html: courseAsListItem,
+                // html: courseAsListItem,
                 id : courseID
             };
             matchingCourses.push(course);
         }
         return matchingCourses;
-    },
-    parseCourseData: function (data) {
-        var course = {
-            courseCode: data.Abbr,
-            courseFullName: data.Name,
-            campus: data.Campus,
-            id: data.ID,
-            semester: data.Semester,
-            sections: data.Sections
-        };
-        return course;
     }
-};
+}
