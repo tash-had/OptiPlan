5fbcd7f60ab5d5c4fe323899665a99c76c053800
Tash-had Saqif <me@tash-had.com>
2018-07-28 12:42:39 -0400

    Almost finished dropdown code

--- ui/main.js
+++ ui/main.js
@@ -3,67 +3,89 @@ var COURSE_MATCHER = new RegExp("^[a-zA-Z]{3}[0-9]{1,3}$");
 
 $(document).ready(function () {
     var input = '', lastSearch = '';
-    $('#courseSearch').keyup(function (e) {
+
+    String.prototype.replaceAll = function(search, replacement) {
+        var target = this;
+        return target.replace(new RegExp(search, 'g'), replacement);
+    };
+
+    $('#courseSearch').on('input', function (e) {
         input = this.value;
-        if (input && input != lastSearch && input.length > 3 && COURSE_MATCHER.test(input)) {
+        if (input && input.length > 3 && input != lastSearch && COURSE_MATCHER.test(input)) {
             lastSearch = input;
             var URL = SEARCH_COURSE_API + input;
-            displayDropdown([]);
             $.get(URL, function (response) {
                 displayDropdown(response);
             });
             return true;
         }
-
-        e.preventDefault();
         return false;
     });
 });
 
-function displayDropdown(arr) {
-    console.log("GO"); 
-    $('.dialog').append('<div>' + "ABC" + '</div>');        
-    var alreadyFilled = false;
-    for (var key of arr) {
-        $('.dialog').append('<div>' + key.courseCode + '</div>');
-    }
-    function clearDialog() {
+function displayDropdown(dataArr) {
+    var currentlySelected = -1; 
+    var numOptions = 0; 
+    var dropdownOpen = false; 
+
+    initDialog(dataArr);
+
+    function initDialog(courses) {
+        numOptions = courses.length; 
         $('.dialog').empty();
+        for (var course of courses) {
+            $('.dialog').append('<div>' + course.courseFullName.replaceAll(":", "") + '</div>');
+        }
+        $('.dialog').addClass('open');
+        dropdownOpen = true; 
+        trackArrowKeys(); 
     }
 
-    $('.autocomplete input').click(function () {
-        if (!alreadyFilled) {
-            $('.dialog').addClass('open');
+    function trackArrowKeys() {
+        $('.autocomplete').keydown(function (e) {
+            e.stopPropagation();
+            switch (e.which) {
+                case 38: // up
+                    selectOption(-1);
+                    break;
+                case 40: // down
+                    selectOption(1);
+                    break;
+
+                default: return;
+            }
+        });
+    }
+
+    function selectOption(change){
+        if (currentlySelected < 1 && change < 1 ||
+            currentlySelected === numOptions-1 && change > -1 ||
+            !dropdownOpen){
+                return; 
         }
-    });
+        var scrollDivisor = 1.20; 
+        if (change == -1){
+            scrollDivisor = 2; 
+        }
+        var options = $('.dialog > div'); 
+        options.eq(currentlySelected).removeClass('selected'); 
+        options.eq(currentlySelected).removeAttr('id');
+        currentlySelected += change; 
+        var newSelection = options.eq(currentlySelected); 
+        newSelection.addClass('selected');
+        newSelection.attr('id', 'selectedItem');
+        $('.dialog').animate({
+            scrollTop: $("#selectedItem").offset().top / scrollDivisor
+        }, 100);
+        $('.autocomplete input').val(newSelection[0].innerHTML);
+    }
 
-    $('body').on('click', '.dialog > div', function () {
+    $('body').on('click', '.dialog > div', function (e) {
         $('.autocomplete input').val($(this).text()).focus();
-        $('.autocomplete .close').addClass('visible');
-        console.log("CLICKED"); 
-        alreadyFilled = true;
-    });
-
-    $('.autocomplete .close').click(function () {
-        alreadyFilled = false;
-        $('.dialog').addClass('open');
-        $('.autocomplete input').val('').focus();
-        $(this).removeClass('visible');
-        console.log("CLOSED"); 
-    });
-
-    $('.autocomplete input').on('input', function () {
-        $('.dialog').addClass('open');
-        alreadyFilled = false;
-        console.log("OPENED"); 
-
     });
 
     $('body').click(function (e) {
-        if (!$(e.target).is("input, .close")) {
-            $('.dialog').removeClass('open');
-        console.log("DESTROYED"); 
-        }
+        $('.dialog').removeClass('open');
+        dropdownOpen = false; 
     });
-    // initDialog();
 }
