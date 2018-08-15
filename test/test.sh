0e0076e00ed0cd0a38bf57c777402faa1fa7f0d0
Tash-had Saqif <me@tash-had.com>
2018-08-14 23:30:56 -0400

    update test script

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,8 @@
-node_modules/.bin/cypress run
+# if no args are provided, run ui test. if args are provided, open cypress 
+
+if [ $# -eq 0 ]
+  then
+    node_modules/.bin/cypress run
+    exit 1
+fi
+node_modules/.bin/cypress open
