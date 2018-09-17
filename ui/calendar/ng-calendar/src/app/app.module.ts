e6393fa58e89dd7c9e5dbca8cd40ca6cf09b5761
Labib <labibc01@gmail.com>
2018-09-16 22:27:50 -0400

    removed angular-calendar-week-hours-view code yet the calendar still works, not sure why

--- ui/calendar/ng-calendar/src/app/app.module.ts
+++ ui/calendar/ng-calendar/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { CalendarWeekHoursViewModule } from 'angular-calendar-week-hours-view'
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
@@ -21,8 +20,7 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
-    }),
-    CalendarWeekHoursViewModule
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
